42c139a003950401f016cc9e4cd1836dad8b874d
Akhil Langer <akhil.langer@intel.com>
2019-01-12 16:42:39 -0600

    mpi_overlapp: Format code using clang-format
    ./clang-format -style=file -i jacobi.cpp jacobi_kernels.cu

--- mpi_overlapp/jacobi_kernels.cu
+++ mpi_overlapp/jacobi_kernels.cu
@@ -28,96 +28,93 @@
 
 #ifdef HAVE_CUB
 #include <cub/block/block_reduce.cuh>
-#endif //HAVE_CUB
+#endif  // HAVE_CUB
 
-#define CUDA_RT_CALL( call )                                                                        \
-{                                                                                                   \
-    cudaError_t cudaStatus = call;                                                                  \
-    if ( cudaSuccess != cudaStatus )                                                                \
-        fprintf(stderr, "ERROR: CUDA RT call \"%s\" in line %d of file %s failed with %s (%d).\n",  \
-                        #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus), cudaStatus);     \
-}
+#define CUDA_RT_CALL(call)                                                     \
+    {                                                                          \
+        cudaError_t cudaStatus = call;                                         \
+        if (cudaSuccess != cudaStatus)                                         \
+            fprintf(stderr,                                                    \
+                    "ERROR: CUDA RT call \"%s\" in line %d of file %s failed " \
+                    "with "                                                    \
+                    "%s (%d).\n",                                              \
+                    #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus), \
+                    cudaStatus);                                               \
+    }
 
 #ifdef USE_DOUBLE
-    typedef double real;
-    #define MPI_REAL_TYPE MPI_DOUBLE
+typedef double real;
+#define MPI_REAL_TYPE MPI_DOUBLE
 #else
-    typedef float real;
-    #define MPI_REAL_TYPE MPI_FLOAT
+typedef float real;
+#define MPI_REAL_TYPE MPI_FLOAT
 #endif
 
-__global__ void initialize_boundaries(
-    real* __restrict__ const a_new,
-    real* __restrict__ const a,
-    const real pi,
-    const int offset,
-    const int nx, const int my_ny, const int ny )
-{
-    for (int iy = blockIdx.x * blockDim.x + threadIdx.x; 
-         iy < my_ny; 
+__global__ void initialize_boundaries(real* __restrict__ const a_new,
+                                      real* __restrict__ const a, const real pi,
+                                      const int offset, const int nx,
+                                      const int my_ny, const int ny) {
+    for (int iy = blockIdx.x * blockDim.x + threadIdx.x; iy < my_ny;
          iy += blockDim.x * gridDim.x) {
-        const real y0 = sin( 2.0 * pi * (offset+iy) / (ny-1) );
-        a[     iy*nx + 0 ]      = y0;
-        a[     iy*nx + (nx-1) ] = y0;
-        a_new[ iy*nx + 0 ]      = y0;
-        a_new[ iy*nx + (nx-1) ] = y0;
+        const real y0 = sin(2.0 * pi * (offset + iy) / (ny - 1));
+        a[iy * nx + 0] = y0;
+        a[iy * nx + (nx - 1)] = y0;
+        a_new[iy * nx + 0] = y0;
+        a_new[iy * nx + (nx - 1)] = y0;
     }
 }
 
-void launch_initialize_boundaries(
-    real* __restrict__ const a_new,
-    real* __restrict__ const a,
-    const real pi,
-    const int offset,
-    const int nx, const int my_ny, const int ny )
-{
-    initialize_boundaries<<<my_ny/128+1,128>>>( a_new, a, pi, offset, nx, my_ny, ny );
-    CUDA_RT_CALL( cudaGetLastError() );
+void launch_initialize_boundaries(real* __restrict__ const a_new,
+                                  real* __restrict__ const a, const real pi,
+                                  const int offset, const int nx,
+                                  const int my_ny, const int ny) {
+    initialize_boundaries<<<my_ny / 128 + 1, 128>>>(a_new, a, pi, offset, nx,
+                                                    my_ny, ny);
+    CUDA_RT_CALL(cudaGetLastError());
 }
 
-template<int BLOCK_DIM_X, int BLOCK_DIM_Y>
-__global__ void jacobi_kernel(
-          real* __restrict__ const a_new,
-    const real* __restrict__ const a,
-          real* __restrict__ const l2_norm,
-    const int iy_start, const int iy_end,
-    const int nx)
-{
+template <int BLOCK_DIM_X, int BLOCK_DIM_Y>
+__global__ void jacobi_kernel(real* __restrict__ const a_new,
+                              const real* __restrict__ const a,
+                              real* __restrict__ const l2_norm,
+                              const int iy_start, const int iy_end,
+                              const int nx) {
 #ifdef HAVE_CUB
-    typedef cub::BlockReduce<real,BLOCK_DIM_X,cub::BLOCK_REDUCE_WARP_REDUCTIONS,BLOCK_DIM_Y> BlockReduce;
+    typedef cub::BlockReduce<real, BLOCK_DIM_X,
+                             cub::BLOCK_REDUCE_WARP_REDUCTIONS, BLOCK_DIM_Y>
+        BlockReduce;
     __shared__ typename BlockReduce::TempStorage temp_storage;
-#endif //HAVE_CUB
-    int iy = blockIdx.y * blockDim.y + threadIdx.y + iy_start; 
-    int ix = blockIdx.x * blockDim.x + threadIdx.x + 1; 
+#endif  // HAVE_CUB
+    int iy = blockIdx.y * blockDim.y + threadIdx.y + iy_start;
+    int ix = blockIdx.x * blockDim.x + threadIdx.x + 1;
     real local_l2_norm = 0.0;
-    
-    if(iy < iy_end && ix < (nx - 1)) {
-        const real new_val = 0.25 * ( a[ iy * nx + ix + 1 ] + a[ iy * nx + ix - 1 ]
-                                    + a[ (iy+1) * nx + ix ] + a[ (iy-1) * nx + ix ] );
-        a_new[ iy * nx + ix ] = new_val;
-        real residue = new_val - a[ iy * nx + ix ];
+
+    if (iy < iy_end && ix < (nx - 1)) {
+        const real new_val =
+            0.25 * (a[iy * nx + ix + 1] + a[iy * nx + ix - 1] +
+                    a[(iy + 1) * nx + ix] + a[(iy - 1) * nx + ix]);
+        a_new[iy * nx + ix] = new_val;
+        real residue = new_val - a[iy * nx + ix];
         local_l2_norm += residue * residue;
     }
 #ifdef HAVE_CUB
     real block_l2_norm = BlockReduce(temp_storage).Sum(local_l2_norm);
-    if ( 0 == threadIdx.y && 0 == threadIdx.x )
-        atomicAdd( l2_norm, block_l2_norm );
+    if (0 == threadIdx.y && 0 == threadIdx.x) atomicAdd(l2_norm, block_l2_norm);
 #else
-    atomicAdd( l2_norm, local_l2_norm );
-#endif //HAVE_CUB
+    atomicAdd(l2_norm, local_l2_norm);
+#endif  // HAVE_CUB
 }
 
-void launch_jacobi_kernel(
-          real* __restrict__ const a_new,
-    const real* __restrict__ const a,
-          real* __restrict__ const l2_norm,
-    const int iy_start, const int iy_end,
-    const int nx,
-    cudaStream_t stream)
-{
+void launch_jacobi_kernel(real* __restrict__ const a_new,
+                          const real* __restrict__ const a,
+                          real* __restrict__ const l2_norm, const int iy_start,
+                          const int iy_end, const int nx, cudaStream_t stream) {
     constexpr int dim_block_x = 32;
     constexpr int dim_block_y = 4;
-    dim3 dim_grid((nx - 1) / dim_block_x + 1, (iy_end-iy_start) / dim_block_y + 1, 1);
-    jacobi_kernel<dim_block_x,dim_block_y><<<dim_grid,{dim_block_x,dim_block_y,1},0,stream>>>( a_new, a, l2_norm, iy_start, iy_end, nx );
-    CUDA_RT_CALL( cudaGetLastError() );
+    dim3 dim_grid((nx - 1) / dim_block_x + 1,
+                  (iy_end - iy_start) / dim_block_y + 1, 1);
+    jacobi_kernel<dim_block_x, dim_block_y>
+        <<<dim_grid, {dim_block_x, dim_block_y, 1}, 0, stream>>>(
+            a_new, a, l2_norm, iy_start, iy_end, nx);
+    CUDA_RT_CALL(cudaGetLastError());
 }
