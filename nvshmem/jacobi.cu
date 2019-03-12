0e0790214f0c6a3c6db23e4b0490a8f141d8cc43
Akhil Langer <alanger@nvidia.com>
2019-03-11 18:37:05 -0700

    nvshmem: Fix grid size calculation

--- nvshmem/jacobi.cu
+++ nvshmem/jacobi.cu
@@ -327,7 +327,7 @@ int main(int argc, char* argv[]) {
 
     constexpr int dim_block_x = 32;
     constexpr int dim_block_y = 4;
-    dim3 dim_grid((nx - 1) / dim_block_x + 1, (ny - 1) / (npes * dim_block_y) + 1, 1);
+    dim3 dim_grid((nx + dim_block_x - 1) / dim_block_x, (chunk_size + dim_block_y - 1) / dim_block_y, 1);
 
     int iter = 0;
     if (!mype) {
